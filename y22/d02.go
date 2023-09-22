b6be6b83c75bc28c6d86a269fba32175ff21a075
EddTheSDET <145542309+EddTheSDET@users.noreply.github.com>
2023-09-22 21:26:41 +0200

    Update d02.go

--- y22/d02.go
+++ y22/d02.go
@@ -70,16 +70,16 @@ func Day02Part2() {
 
 	/*
 		A, X = Lose + Scissors	= 0 + 3 = 3
-		A, Y = Draw + Rock		= 3 + 1 = 4
-		A, Z = Win  + Paper		= 6 + 2 = 8
+		A, Y = Draw + Rock	= 3 + 1 = 4
+		A, Z = Win  + Paper	= 6 + 2 = 8
 
-		B, X = Lose + Rock		= 0 + 1 = 1
-		B, Y = Draw + Paper		= 3 + 2 = 5
+		B, X = Lose + Rock	= 0 + 1 = 1
+		B, Y = Draw + Paper	= 3 + 2 = 5
 		B, Z = Win  + Scissors	= 6 + 3 = 9
 
-		C, X = Lose + Paper		= 0 + 2 = 2
+		C, X = Lose + Paper	= 0 + 2 = 2
 		C, Y = Draw + Scissors	= 3 + 3 = 6
-		C, Z = Win  + Rock		= 6 + 1 = 7
+		C, Z = Win  + Rock	= 6 + 1 = 7
 
 	*/
 
