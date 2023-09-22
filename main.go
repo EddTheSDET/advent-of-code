3f0658b701c34dfc42e02f2c7936bf671ea51e06
EddTheSDET <eddthesdet@gmail.com>
2023-09-22 21:25:23 +0200

    Complete y22d2p2

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "advent-of-code/y22"
 
 func main() {
-	y22.Day02()
+	y22.Day02Part1()
+	y22.Day02Part2()
 }
--- y22/d02.go
+++ y22/d02.go
@@ -2,7 +2,7 @@ package y22
 
 import "advent-of-code/utils"
 
-func Day02() {
+func Day02Part1() {
 	scanner := utils.OpenDataFile("y22/d02.data")
 
 	totalPoints := 0
@@ -57,6 +57,60 @@ func Day02() {
 		}
 		if current_line == "C Z" {
 			totalPoints += 6
+		}
+	}
+
+	println(totalPoints)
+}
+
+func Day02Part2() {
+	scanner := utils.OpenDataFile("y22/d02.data")
+
+	totalPoints := 0
+
+	/*
+		A, X = Lose + Scissors	= 0 + 3 = 3
+		A, Y = Draw + Rock		= 3 + 1 = 4
+		A, Z = Win  + Paper		= 6 + 2 = 8
+
+		B, X = Lose + Rock		= 0 + 1 = 1
+		B, Y = Draw + Paper		= 3 + 2 = 5
+		B, Z = Win  + Scissors	= 6 + 3 = 9
+
+		C, X = Lose + Paper		= 0 + 2 = 2
+		C, Y = Draw + Scissors	= 3 + 3 = 6
+		C, Z = Win  + Rock		= 6 + 1 = 7
+
+	*/
+
+	for scanner.Scan() {
+		current_line := scanner.Text()
+		if current_line == "A X" {
+			totalPoints += 3
+		}
+		if current_line == "A Y" {
+			totalPoints += 4
+		}
+		if current_line == "A Z" {
+			totalPoints += 8
+		}
+		if current_line == "B X" {
+			totalPoints += 1
+		}
+		if current_line == "B Y" {
+			totalPoints += 5
+		}
+		if current_line == "B Z" {
+			totalPoints += 9
+		}
+		if current_line == "C X" {
+			totalPoints += 2
+		}
+		if current_line == "C Y" {
+			totalPoints += 6
+		}
+		if current_line == "C Z" {
+			totalPoints += 7
 		}
 	}
 
